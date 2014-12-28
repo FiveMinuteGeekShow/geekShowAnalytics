3e83e238e9ec6aa9c6e28deead568e77725476c1
Matt Stauffer <matt@tighten.co>
2014-12-28 00:31:01 -0500

    Drop analytics on public page

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -2,8 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>The Five Minute Geek Show</title>
-    <meta name="description" content="The Five-Minute Geek Show: Matt Stauffer, unabashedly geeky, 5 minutes, twice a week.">
+    <title>Analytics for The Five Minute Geek Show</title>
     <meta name="author" content="Matt Stauffer">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,7 +14,6 @@
         }
 
         body {
-            border-top: 140px solid #cee7d8;
             color: #666;
             font-family: arial, sans-serif;
             margin: 0;
@@ -28,11 +26,6 @@
             width: 90%;
         }
 
-        .logo {
-            margin-top: -175px;
-            width: 561px;
-        }
-
         a, a:visited {
             text-decoration:none;
         }
@@ -42,21 +35,7 @@
             border: none;
             height: 1px;
         }
-
-        .videos-list {
-            text-align: left;
-        }
     </style>
-
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-40114814-4', 'auto');
-        ga('send', 'pageview');
-    </script>
 </head>
 <body>
      @yield('content')
